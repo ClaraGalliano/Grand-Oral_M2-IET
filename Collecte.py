1b9de22a137a154d71089698f216c5dcc727e4a0
Clara Galliano <46959041+ClaraGalliano@users.noreply.github.com>
2023-01-22 21:03:35 +0100

    Update Collecte.py

--- Collecte.py
+++ Collecte.py
@@ -15,7 +15,7 @@ Dern= False
 cpt = 0
 with open('nazanin.pkl', 'w') as fic:
     while not Dern or cpt>400000:
-        url='https://www.change.org/api-proxy/-/petitions/libérez-nazanin-ratcliffe/comments?limit=10&offset='+str(cpt)+"&order_by=voting_score"
+        url='https://www.change.org/api-proxy/-/petitions/lu-stop-à-l-utilisation-d-huile-de-palme/comments?limit=10&offset='+str(cpt)+"&order_by=voting_score"
         page = requests.get(url)
         if page.ok:
             data = json.loads(page.text)
