877023d0ad45a2636fc6f1effcc484ef84e3ffbf
Clara Galliano <46959041+ClaraGalliano@users.noreply.github.com>
2023-01-22 21:08:40 +0100

    Update Collecte.py

--- Collecte.py
+++ Collecte.py
@@ -13,7 +13,7 @@ import pickle # bibliothèque de serialisation
 Res = ''
 Dern= False
 cpt = 0
-with open('nazanin.pkl', 'w') as fic:
+with open('hpalme.pkl', 'w') as fic:
     while not Dern or cpt>400000:
         url='https://www.change.org/api-proxy/-/petitions/lu-stop-à-l-utilisation-d-huile-de-palme/comments?limit=10&offset='+str(cpt)+"&order_by=voting_score"
         page = requests.get(url)
